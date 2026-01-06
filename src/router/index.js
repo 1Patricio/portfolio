283084c1ec43c3ef30c4014a99217758b4bc397c
Anderson Patricio <andersongpatricio@hotmail.com>
2026-01-06 14:47:01 -0300

    feat: alterando para create Web Hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import ProjetcsPage from '@/pages/ProjetcsPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
